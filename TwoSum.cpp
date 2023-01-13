02b750a4d30a148c2ff23f8c9a63835409442ef2
Ishita Srivastav <104553220+Ishu-sri-001@users.noreply.github.com>
2023-01-13 21:33:00 +0530

    Create TwoSum.cpp

---
+++ TwoSum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i=0;i<nums.size();i++)
+        {
+            for (int j=0;j<i;j++)
+            {
+                if ((nums[i]+nums[j])==target)
+                {
+                    return{i,j};
+                }
+            }
+        }
+        return{};
+    }
+};
