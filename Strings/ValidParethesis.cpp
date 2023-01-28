5ebc15ca62d624557e701dde8b4a7638b4825d92
Ishita Srivastav <104553220+Ishu-sri-001@users.noreply.github.com>
2023-01-28 21:30:22 +0530

    Create ValidParethesis.cpp

---
+++ Strings/ValidParethesis.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        if (s.length()==1)
+        return false;
+        else if (s.length()==0)
+        return true;
+        for (int i=0;i<s.length();i++)
+        {
+            char c=s.at(i);
+            if (c=='{' || c=='[' || c=='(')
+                st.push(c);
+            else {
+                if (st.empty())
+                    return false;
+                if (c == '}' && st.top() == '{')
+                    st.pop();
+                else if (c == ')' && st.top() == '(')
+                    st.pop();
+                else if (c == ']' && st.top() == '[')
+                    st.pop();
+                else
+                    return false;
+            }
+        }
+        return st.empty();
+    }
+};
