abc1d9abde89b0c3e856eddf26e11d72df752236
Ishita Srivastav <104553220+Ishu-sri-001@users.noreply.github.com>
2023-05-05 12:26:31 +0530

    Create MaxNoOfVowels.cpp

---
+++ MaxNoOfVowels.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/
+
+class Solution {
+public:
+    int maxVowels(string s, int k) {
+        int n = s.length();
+        int c = 0;
+        int mc = 0;
+        for(int i=0; i<k; i++) {
+            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+                c++;
+        }
+        mc=c;
+        for(int i=k; i<n; i++) {
+            if(s[i-k] == 'a' || s[i-k] == 'e' || s[i-k] == 'i' || s[i-k] == 'o' || s[i-k] == 'u')
+                c--;
+            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+                c++;
+            mc = max(mc, c);
+        }
+        return mc;
+    }
+};
