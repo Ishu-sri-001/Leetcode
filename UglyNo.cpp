4f8fb6510830353a910d94fad261343cd042494d
Ishita Srivastav <104553220+Ishu-sri-001@users.noreply.github.com>
2023-05-29 21:58:08 +0530

    Create UglyNo.cpp

---
+++ UglyNo.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/ugly-number/description/
+
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n<1)
+            return false;
+        while(n%2==0)
+            n=n/2;
+        while(n%3==0)
+            n=n/3;
+        while(n%5==0)
+            n=n/5;
+        return n==1;
+    }
+};
