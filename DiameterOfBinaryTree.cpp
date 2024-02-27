3eb9a5a8774827c5eaa2c3bbf4a278463d396d4f
Ishita Srivastav <104553220+Ishu-sri-001@users.noreply.github.com>
2024-02-27 13:04:10 +0530

    Create DiameterOfBinaryTree.cpp

---
+++ DiameterOfBinaryTree.cpp
@@ -0,0 +1,30 @@
+//https://leetcode.com/problems/diameter-of-binary-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int helper(TreeNode* root, int& diameter )
+    {
+        if(root==NULL)
+            return 0;
+        int left=helper(root->left,diameter);
+        int right=helper(root->right,diameter);
+        diameter=max(diameter,left+right);
+        return max(left,right)+1;
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        int diameter=0;
+        helper(root,diameter);
+        return diameter;
+    }
+};
